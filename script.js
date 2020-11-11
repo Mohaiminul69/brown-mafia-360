04bb10fc27b3328f237a49780107022d11b64ca0
Mohaiminul69 <70589852+Mohaiminul69@users.noreply.github.com>
2020-11-11 17:00:31 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let slideIndex = 0;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("yes");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
+carousel();
+
+function carousel() {
+  let i;
+  let x = document.getElementsByClassName("yes");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(carousel, 7000);
+}
+
+$(".btn").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    const hash = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800
+    );
+  }
+});
